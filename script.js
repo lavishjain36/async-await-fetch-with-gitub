414adf38a32605886e6e511f6f1136d40774fa5f
Lavish <lavishjain36@gmail.com>
2021-12-26 11:57:19 +0530

    Github Async await

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-const cat_btn=document.getElementById('cat_btn');
-const cat_result=document.getElementById('cat_result');
-const dog_result=document.getElementById('dog_result');
-const dog_btn=document.getElementById('dog_btn');
+//select both the target
+const fetchDataBtn=document.querySelector('#fetchdata');
+const result=document.querySelector('#result');
 
-
-cat_btn.addEventListener('click',getRandomCat);
-
-//is user click on button ,it should call an callback function getRandomDog
-dog_btn.addEventListener('click',getRandomDog);
-
-function getRandomCat(){
- fetch('https://aws.random.cat/meow').
- then((res)=>res.json())
- .then((data)=>{
-      // console.log(data);
-      cat_result.innerHTML=`<img src="${data.file}" alt="cat" width="400" height="200">`;
-
-  }).catch((err)=>{
-      console.log(err);
-  });
+//Get the data from an api and set the contents of the result div accordingly
+async function getData(){
+result.innerHTML="Loading...";
+try{
+const res=await fetch('https://api.github.com/users/lavishjain36')
+const jsonResult=await res.json();
+result.innerHTML=`
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">${jsonResult.name}</h5>
+<h6 class="card-subtitle mb-2 text-muted">${jsonResult.bio}</h6>
+<p class="card-text">${jsonResult.location}</p>
+<a href="${jsonResult.html_url}" class="card-link">Github Profile</a>
+</div>
+</div>
+`;
+}catch(error){
+    result.innerHTML=error;
+}
 }
 
-function getRandomDog(){
-fetch("https://random.dog/woof.json")
-.then((res)=>res.json())
-.then((data)=>{
-  console.log(data);
-  //checking if the data contains .mp4 then display video else display image
-  if(data.url.includes("mp4")){
-    dog_result.innerHTML=`<video width="400" height="200" controls>
-    <source src="${data.url}" type="video/mp4">
-    </video>`;
-  }
-  else{
-    dog_result.innerHTML=`<img src="${data.url}" alt="dog" width="400" height="200">`;
-  }
-}).catch((err)=>{
-  console.log(err);
-});
-}
+
+fetchDataBtn.addEventListener('click',getData);
